216c35d0ad364e32ed684f56eb132f9c2979c66f
Chris Bieganek <cwbieganek@gmail.com>
2022-04-29 18:54:56 -0500

    Add support for quote-delimited fields. Parse character-by-character.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub struct CsvParser {
 impl CsvParser {
     // This currently assumes there are no commas in cells (for now).
     // It will also strip " from the start and end of cells (for now).
-    pub fn new(csv_path: &String) -> Self {
+    pub fn new_old_version(csv_path: &String) -> Self {
         let file_contents = fs::read_to_string(&csv_path).expect("Failed to read CSV.");
         let mut lines = file_contents.lines();
         let mut header: Vec<String> = Vec::new();
@@ -42,6 +42,83 @@ impl CsvParser {
             rows: rows.clone()
         }
     }
+
+    pub fn new(csv_path: &String) -> Self {
+        let file_contents = fs::read_to_string(&csv_path).expect("Failed to read CSV.");
+        let mut lines = file_contents.lines();
+        let header: Vec<String>;
+        let mut rows: Vec<Vec<String>> = Vec::new();
+
+        // Start with the header of the CSV
+        if let Some(first_line) = lines.next() {
+            header = parse_line(first_line);
+        } else {
+            panic!("CSV has no data.");
+        }
+
+        for row in lines {
+            rows.push(parse_line(row));
+        }
+
+        Self {
+            header: header.clone(),
+            rows: rows.clone()
+        }
+    }
+}
+
+fn parse_line(line: &str) -> Vec<String> {
+    let mut row: Vec<String> = Vec::new();
+    let mut in_cell = true;
+    let mut in_quoted_cell = false;
+
+    // If the very first cell is quote-delimited, switch in_cell to false
+    // This will guarantee that the first " will be ignored.
+    if let Some(first_char) = line.chars().next() {
+        if first_char == '"' {
+            // Very first cell is quote-delimited
+            in_cell = false;
+        }
+    }
+
+    let mut cell = String::new();
+    
+    for char in line.chars() {
+        if char == '\"' {
+            if in_cell {
+                // Found the end of a quote-delimited cell
+                // Note that this means we cannot support quotes inside fields
+                in_cell = false;
+                in_quoted_cell = false;
+
+                row.push(cell.clone());
+                cell = String::new();
+            } else {
+                // We are now in a quote-delimited cell
+                in_cell = true;
+                in_quoted_cell = true;
+            }
+        } else if char == '\n' {
+            // Must be the end of the line
+            return row;
+        } else if char == ',' {
+            if in_quoted_cell {
+                cell.push(char);
+            } else {
+                // End of non-quoted cell
+                in_cell = false;
+                row.push(cell.clone());
+                cell = String::new();
+            }
+        } else {
+            cell.push(char);
+        }
+    }
+
+    // Reached end of line. Push current cell contents.
+    row.push(cell.clone());
+
+    row
 }
 
 pub enum CellType {
